44a9a1f5a4f2651c42bc9a495b183455f18c1504
Morlaye Cisse <morlayecis0003@gmail.com>
2023-12-07 21:27:46 +0000

    configuration du projet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideRouter } from '@angular/router';
 
+import { environment } from 'src/environments/environment.development';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes),
+    //definir la configurartion de notre projet firebase
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideStorage(() => getStorage()), 
+    //initialisatin de mon application en ligne et en locale
+    provideFirestore(() => initializeFirestore(getApp(), {
+        localCache: persistentLocalCache({
+            tabManager: persistentMultipleTabManager()
+        })
+    }))), provideAnimations()]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAT8oGq-lSlpGJr3kfa8j5FvzP1lJrmJAs",
+        authDomain: "marcher-demo.firebaseapp.com",
+        projectId: "marcher-demo",
+        storageBucket: "marcher-demo.appspot.com",
+        messagingSenderId: "824511392410",
+        appId: "1:824511392410:web:4d4e64d5359fb4e7280d23"
+      }
+};
